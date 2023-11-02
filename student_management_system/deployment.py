c44397aa62e2cfb9bc815f59d47764f16ea32fed
kamanzialponse <Kamanzialphonse46@gmail.com>
2023-11-02 14:30:19 +0200

    change deployment file

--- student_management_system/deployment.py
+++ student_management_system/deployment.py
@@ -35,3 +35,16 @@ DATABASES = {
         'PASSWORD': conn_str_params['password'],
     }
 }
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
